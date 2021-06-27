a2c0fdc2f78bf9991e31cf9f05cbe65639132808
imageprocessing-hiof <bjorn.s.andersson@hiof.no>
2021-06-27 17:38:19 +0200

    Latest

--- image_processing.py
+++ image_processing.py
@@ -30,6 +30,8 @@ red_threshold = (45, 79, 39, 90, -7, 78)
 green_threshold = (31, 81, -88, -27, -51, 41)
 blue_threshold = (9, 66, -20, 85, -128, -14)
 
+global angle_list
+angle_list=list()
 
 # color of lines drawn around min area
 r = (pyb.rng() % 127) + 128
@@ -56,6 +58,7 @@ def family_name(tag):
         return "TAG36H11"
     if(tag.family() == image.ARTOOLKIT):
         return "ARTOOLKIT"
+
 
 # Detecting April tags and return them in an april_tag dictionary
 def calibration():
@@ -136,8 +139,87 @@ def find_len_of_blobs(blobs):
     return length_of_blobs_list
 
 
-def draw_blobs(blobs):
+def get_blob_data(blobs,data_dict):
 
+    total_count = 0
+    for color_code, color in enumerate(blobs):
+        for blob_count, blob in enumerate(color):
+            total_count = total_count + 1
+            color = get_color(color_code)
+            cx = blob.cx()
+            cy = blob.cy()
+            img.draw_cross(cx,cy, color=255)
+            if total_count in data_dict:
+                data_tuple = data_dict.get(total_count)
+                prev_angle = data_tuple[3]
+                #print(data_dict)
+                #prev_angle = data_dict.get(total_count)[3]
+            else:
+                prev_angle = -1
+
+            #print(data_dict)
+            angle_of_rotation = get_angle_of_rotation_tan(blob, prev_angle)
+            is_cuboid = is_cuboid_or_rectangle(blob)
+            data_dict[total_count] = (color, cx, cy, angle_of_rotation, is_cuboid)
+
+    return data_dict
+
+
+def get_color(color_code):
+    if (color_code == 0):
+        return "White"
+    elif (color_code == 1):
+        return "Red"
+    elif (color_code == 2):
+        return "Green"
+    elif (color_code == 3):
+        return "Blue"
+
+# Threshold of rectangle was found by experimenting.
+def is_cuboid_or_rectangle(blob):
+    if (blob.area() > 750):
+        #print(blob.area())
+        return True
+    elif (blob.area() < 750):
+        return False
+
+def get_angle_of_rotation_tan(blob, prev_degree):
+    y_min = float("inf")
+    x_max = 0
+    degrees=0
+    # The corners of the minimum area rectangle of each blob.
+    corners = blob.min_corners()
+    corner_dict = {}
+    corner_dict[1] = corners[0]
+    corner_dict[2] = corners[1]
+    corner_dict[3] = corners[2]
+    corner_dict[4] = corners[3]
+
+    for cor_id,cor in corner_dict.items():
+        if (cor[1] < y_min):
+            y_min = cor[1]
+            top_corner_id = cor_id
+
+        if (cor[0] > x_max):
+            x_max = cor[0]
+            right_corner_id = cor_id
+
+    ctx,cty = corner_dict[top_corner_id]
+    crx,cry = corner_dict[right_corner_id]
+
+    angle = math.atan2(cry - cty, crx - ctx)
+    degrees = ( angle * (180 / math.pi) )
+
+    if(prev_degree == -1):
+        prev_degree = degrees
+
+    #Using Exponential filter to filter the noise in the angle
+    w=0.10
+    angle = (w * degrees) + ((1-w) * prev_degree)
+    return int(angle)
+
+
+def draw_blobs(blobs):
     for color in blobs:
         for blob in color:
             # The corners of the minimum area rectangle of each blob.
@@ -164,7 +246,10 @@ def draw_blobs(blobs):
 ##########################################################
 
 april_tags_dict = {}
+data_dict = {}
 april_tags_dict , calib_result = calibration()
+counter = 0
+prev_degree = -1
 
 #Check for Calibration malfunction and retry
 for iter in range(10):
@@ -184,30 +269,30 @@ if (calib_result):
     sensor.set_auto_gain(False) # must be turned off for color tracking
     sensor.set_auto_whitebal(False) # must be turned off for color tracking
 
-
     x,y,w,h = get_roi(april_tags_dict)
     X,Y,W,H = upscale_QQVGA_to_QVGA(x,y,w,h)
 
     # Draw a rectangle to mark the ROI boundary
-    img.draw_rectangle(X,Y,W,H, color=255)
+    #img.draw_rectangle(X,Y,W,H, color=255)
 
     while(True):
         clock.tick()
+        #tick1= time.ticks_us()
         img = sensor.snapshot().lens_corr(strength = 1.8, zoom = 1.0)
         mask_april_tags(april_tags_dict)
         blobs = find_blobs(X,Y,W,H)
         len_blobs_list = find_len_of_blobs(blobs)
         draw_blobs(blobs)
-        print(len_blobs_list)
+        if(counter==50):
+            data_dict.clear()
+            counter = 0
+        else:
+            counter = counter + 1
+        data_dict = get_blob_data(blobs,data_dict)
+
+        print(img.compressed_for_ide())
+        print(data_dict)
+
 else:
     print("Calibration Failed!!!. \n Please check camera for lens focus, overheat or april tags obstruction")
-
-
-
-
-
-
-
-
-
 
